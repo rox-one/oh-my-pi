d14875ad0956c0fffc8db830a8e3bada86c59dab
Luiz Ferraz <luiz@lferraz.com>
2026-06-29 19:31:00 +0000

    no-mistakes(lint): Clean lint and type checks

--- packages/coding-agent/src/utils/__tests__/git-remotes.test.ts
+++ packages/coding-agent/src/utils/__tests__/git-remotes.test.ts
@@ -2,7 +2,7 @@ import { afterEach, describe, expect, it } from "bun:test";
 import * as fs from "node:fs";
 import * as os from "node:os";
 import * as path from "node:path";
-import { repo, type GitRepository } from "../git";
+import { type GitRepository, repo } from "../git";
 
 const tempDirs: string[] = [];
 
